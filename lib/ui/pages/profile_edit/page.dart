7fce1943764ed45d232303b7399a716c52ebc30d
tttuuu1234 <yidongyi8@gmail.com>
2023-03-06 20:33:15 +0900

    デザイン整え

--- lib/ui/pages/profile_edit/page.dart
+++ lib/ui/pages/profile_edit/page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_sample/ui/components/basic_layout/widget.dart';
 import 'package:chat_sample/ui/styles/color.dart';
+import 'package:chat_sample/ui/styles/margin.dart';
 import 'package:chat_sample/ui/styles/padding.dart';
 import 'package:dotted_border/dotted_border.dart';
 
@@ -17,30 +18,45 @@ class ProfileEditPage extends ConsumerWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          child: const Text('保存'),
+          child: const Text(
+            '保存',
+            style: TextStyle(color: AppColor.white),
+          ),
         ),
       ],
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('メインのプロフィール写真'),
-            const SizedBox(
-              height: 350,
-              child: Placeholder(),
-            ),
-            const Text('あなたに関する写真を8枚まで追加できます。'),
-            SizedBox(
-              height: 64,
-              width: 64,
-              child: DottedBorder(
-                color: AppColor.grey,
-                child: const Center(
-                  child: Icon(
-                    Icons.add,
-                    color: AppColor.grey,
+            AppVerticalMargin.small,
+            Padding(
+              padding: AppPadding.smallHorizontal,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('メインのプロフィール写真'),
+                  AppVerticalMargin.small,
+                  const SizedBox(
+                    height: 350,
+                    child: Placeholder(),
                   ),
-                ),
+                  AppVerticalMargin.small,
+                  const Text('あなたに関する写真を8枚まで追加できます。'),
+                  AppVerticalMargin.small,
+                  SizedBox(
+                    height: 64,
+                    width: 64,
+                    child: DottedBorder(
+                      color: AppColor.grey,
+                      child: const Center(
+                        child: Icon(
+                          Icons.add,
+                          color: AppColor.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                  AppVerticalMargin.small,
+                ],
               ),
             ),
             InputFiledBaseLayout(label: '名前', widget: TextFormField()),
@@ -48,6 +64,7 @@ class ProfileEditPage extends ConsumerWidget {
             InputFiledBaseLayout(label: '年齢', widget: TextFormField()),
             InputFiledBaseLayout(label: '場所', widget: TextFormField()),
             InputFiledBaseLayout(label: '自己紹介', widget: TextFormField()),
+            AppVerticalMargin.small,
           ],
         ),
       ),
---
+++ lib/ui/styles/text_style.dart
@@ -0,0 +1,8 @@
+import 'package:chat_sample/ui/styles/size.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  AppTextStyle._();
+
+  static TextStyle xSmall = TextStyle(fontSize: AppSize.xSmall);
+}
