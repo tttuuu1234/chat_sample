0c911d695494ce359dbaf5f17ce3c9796e9fbf56
tttuuu1234 <yidongyi8@gmail.com>
2023-03-11 14:23:56 +0900

    flavor_androidの設定

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
 void main() {
+  const flavor = String.fromEnvironment('flavor');
+  print(flavor);
   runApp(const ProviderScope(child: App()));
 }
